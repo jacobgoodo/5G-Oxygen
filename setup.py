b6c4ce519ef861e3604c3431757f28e2f4ee4a24
Spotlight <spotlight@joscomputing.space>
2020-06-14 01:01:23 -0500

    Add documentation for easier reading

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import tweepy
 import sys
 from pathlib import Path
 
+# Initialise empty config as a dict to be written to
 config = {}
 
 print("For the next two questions, you'll need an application from https://developer.twitter.com/apps to Tweet with.")
@@ -11,17 +12,19 @@ config["consumer_secret"] = input("Enter your app's consumer secret key: ")
 
 auth = tweepy.OAuthHandler(config["consumer_key"], config["consumer_secret"])
 try:
+	# This will fail if the consumer key/secret is invalid.
 	redirect_url = auth.get_authorization_url()
 	print("Go to the following URL and input the verification code given.")
 	print(redirect_url)
 except tweepy.TweepError:
 	print("Unable to get a request token. Did you configure a valid consumer key and secret?")
 	sys.exit(1)
-	
+
 verification_token = input("Verification token: ")
 	
 # Use given verification token to obtain an access key and secret
 try:
+	# Returned is a tuple with format [token, secret].
 	access_tokens = auth.get_access_token(verification_token)
 	config["access_token"] = access_tokens[0]
 	config["access_secret"] = access_tokens[1]
@@ -29,6 +32,7 @@ except tweepy.TweepError:
 	print("Unable to get access credentials. Did you enter an invalid verification token?")
 	sys.exit(1)
 
+# Write a formatted config
 formatted_config = json.dumps(config, indent = 4)
 config_path = Path("./config.json")
 config_path.write_text(formatted_config)
